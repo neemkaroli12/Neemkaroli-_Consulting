a9aa0df87d6ab0771ab734772de4d754d67988cd
Anshika Chauhan <chauhananshika533@gmail.com>
2025-09-02 16:57:43 +0530

    changes

--- consulting_app/forms.py
+++ consulting_app/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-
+from .models import Estimate
 class CareerApplicationForm(forms.Form):
     name = forms.CharField(max_length=100, label="Full Name", widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
     email = forms.EmailField(label="Email Address", widget=forms.EmailInput(attrs={'placeholder': 'Your Email'}))
     message = forms.CharField(label="Message", widget=forms.Textarea(attrs={'placeholder': 'Write a short message...'}), required=False)
     resume = forms.FileField(label="Upload Resume", required=False)
+
+
+class estimateForm(forms.Form):
+    name = forms.CharField(max_length=100, label="Full Name", widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
+    email = forms.EmailField(label="Email Address", widget=forms.EmailInput(attrs={'placeholder': 'Your Email'}))
+   
--- consulting_app/models.py
+++ consulting_app/models.py
@@ -13,3 +13,17 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
 
+class Estimate(models.Model):
+    name = models.CharField(max_length=200,blank=True,null=True)
+    company_name = models.CharField(max_length=200,blank=True,null=True)
+    location = models.CharField(max_length=200,blank=True,null=True)
+    type = models.CharField(blank=True, null=True)
+    Employees_no =  models.IntegerField(blank=True, null=True)
+    turnover =     models.IntegerField(blank=True, null=True)
+    designation = models.CharField(max_length=200,blank=True,null=True)
+    mobile_no = models.IntegerField(blank=True, null=True)
+    email = models.EmailField()
+    existing_appli = models.CharField(max_length=200,blank=True,null=True)
+    no_of_users = models.IntegerField(blank=True, null=True)
+    product = models.CharField(max_length=200,blank=True,null=True)
+    module = models.CharField(max_length=200,blank=True,null=True)
